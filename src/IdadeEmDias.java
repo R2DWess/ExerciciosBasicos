8b64ff51f07a36f7e12a7d37c84b796805c7d3cf
WesleLima <wzztudy@gmail.com>
2024-04-10 07:20:11 -0300

    Add: comentário de orientação da questão desenvolvida

--- src/IdadeEmDias.java
+++ src/IdadeEmDias.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/* Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+ * mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+ * (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)*/
+
 public class IdadeEmDias {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
